f5f74555f94019a35470c71e1e6a93b89fbcb2ec
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-02-04 21:35:59 -0600

    remove the notifier pattern and instead just keep track of the conns

--- websockets/websockets.go
+++ websockets/websockets.go
@@ -2,9 +2,11 @@ package websockets
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/fsnotify/fsnotify"
 	"nhooyr.io/websocket"
@@ -17,7 +19,11 @@ type websocketServer struct {
 
 	applicationPort int
 
-	notifier *notifier
+	mu sync.Mutex
+
+	// connections is a map from connection to a channel which will be closed when the connection is
+	// closed.
+	connections map[*websocket.Conn]<-chan struct{}
 }
 
 func NewServer(
@@ -29,40 +35,48 @@ func NewServer(
 		events:          events,
 		errors:          errors,
 		applicationPort: applicationPort,
-		notifier:        newNotifier(),
+		connections:     make(map[*websocket.Conn]<-chan struct{}),
 	}
 }
 
-func (s websocketServer) Start(ctx context.Context) {
+func (s *websocketServer) Start(ctx context.Context) {
 	go s.listenToEvents(ctx)
 }
 
-func (s websocketServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *websocketServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
 		OriginPatterns: []string{fmt.Sprintf("localhost:%d", s.applicationPort)},
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer c.CloseNow()
-
 	log.Println("websocket connected")
 
+	// We won't be reading from this channel. The ctx gets closed when the client closes the
+	// connection.
 	ctx := c.CloseRead(r.Context())
 
-	s.notifier.add(func(e fsnotify.Event) error {
-		return s.handleEvent(c, e)
-	})
+	select {
+	case <-ctx.Done():
+		// If the connection has already been closed, skip adding it entirely.
+		return
+	default:
+		s.addConnection(ctx, c)
+	}
 
-	<-ctx.Done()
+	go func() {
+		<-ctx.Done()
+
+		// Once the client is closed, we should remove its connection so we no longer try to notify
+		// it.
+		s.removeConnection(c)
+	}()
 }
 
-func (s websocketServer) listenToEvents(ctx context.Context) {
-	log.Println("websocket server listening for file change events...")
+func (s *websocketServer) listenToEvents(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			log.Println("websocket event listener exiting...")
 			return
 		case err, ok := <-s.errors:
 			if !ok {
@@ -75,7 +89,8 @@ func (s websocketServer) listenToEvents(ctx context.Context) {
 			if !ok {
 				return
 			}
-			err := s.notifier.notify(event)
+
+			err := s.notifyConnections(event)
 			if err != nil {
 				log.Println("error:", err)
 			}
@@ -83,11 +98,46 @@ func (s websocketServer) listenToEvents(ctx context.Context) {
 	}
 }
 
-func (s websocketServer) handleEvent(c *websocket.Conn, event fsnotify.Event) error {
+func (s *websocketServer) notifyConnections(event fsnotify.Event) error {
 	if !event.Op.Has(fsnotify.Write) {
 		return nil
 	}
+	log.Printf("file changed: %s\n", event.Name)
 
-	log.Println("file changed:", event.Name)
-	return wsjson.Write(context.Background(), c, "file change")
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	i := 0
+	errs := make([]error, len(s.connections))
+	for c, done := range s.connections {
+		select {
+		case <-done:
+			// If this connection is closed, our cleanup goroutine will remove it. We shouldn't try
+			// to write to it.
+			continue
+		default:
+		}
+
+		err := wsjson.Write(context.Background(), c, "file change")
+		if err != nil {
+			errs[i] = err
+		}
+		i++
+	}
+
+	return errors.Join(errs...)
+}
+
+func (s *websocketServer) addConnection(ctx context.Context, c *websocket.Conn) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.connections[c] = ctx.Done()
+}
+
+func (s *websocketServer) removeConnection(c *websocket.Conn) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	delete(s.connections, c)
 }
